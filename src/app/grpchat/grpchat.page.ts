40e7b2f4a52fa75d15193f57a103c4c0a5804883
RANYA EL HADRI <ranya_elhadri@um5.ac.ma>
2021-01-12 03:13:53 +0100

    Chat Version 3

--- src/app/grpchat/grpchat.page.ts
+++ src/app/grpchat/grpchat.page.ts
@@ -9,11 +9,47 @@ import{ ChatService } from '../chat.service';
 export class GrpchatPage implements OnInit {
   grpchat;
 
-  constructor(private  service:ChatService) { }
+  messageText: any;
+  messages = [];
+  userId: string;
+
+  constructor(private  service:ChatService) { this.getMessages(); }
 
   ngOnInit() {
     this.grpchat = this.service.currentChatGrp;
     console.log(this.service.currentChatGrp);
+  }
+
+  sendMessage(){
+    console.log('messageText :' + this.service.login)
+    console.log('messageText :' + this.messageText)
+    this.service.firestore.collection('Message').add({
+      userId: this.service.userId,
+      login: this.service.login,
+      text: this.messageText,
+      date: new Date().toISOString(),
+      grpTitle: this.grpchat.title
+    });
+    this.messageText='';
+  }
+
+
+  getMessages() {
+    
+    this.service.firestore.collection("Message", ref => ref.orderBy('date')).snapshotChanges()
+    .subscribe(actions => {
+      this.messages = [];
+      actions.forEach(action => {
+        this.messages.push({
+          userId: action.payload.doc.data()["userId"],
+          login :action.payload.doc.data()["login"],
+          text:  action.payload.doc.data()["text"],
+          date: action.payload.doc.data()["date"],
+          grpTitle: action.payload.doc.data()["grpTitle"]
+        });
+      });
+    });
+     
   }
 
 }
